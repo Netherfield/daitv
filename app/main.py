761d48ab4260f6615b9e7dcee43fc5c6e91c652d
Netherfield <barberisjacopo@gmail.com>
2024-02-14 10:00:28 +0100

    main

--- app/main.py
+++ app/main.py
@@ -6,6 +6,22 @@
 
 def main():
     ...
+    """
+    RIVEDERE NOMI e location funzioni da cambiare
+    """
+    if False:
+        sql_loader()
+
+    if False:
+        csv_cleaner()
+
+    if False:
+        db_loader()
+
+    if False:
+        """more function that we could need"""
+
+
 
 
 
